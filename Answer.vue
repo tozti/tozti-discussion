43150647c6fff8f86350b3beb648237f94367aec
lucasperotin <34893315+lucasperotin@users.noreply.github.com>
2018-02-17 21:39:22 +0100

    Add files via upload
    Component for message, answer of the message and textfield added
    Possibility of showing/hiding answers of messages added

---
+++ Answer.vue
@@ -0,0 +1,30 @@
+<template>
+
+<div style="text-align: left;padding-left: 60px">
+<article class="media">
+  <figure class="media-left">
+    <p class="image is-64x64">
+      <img src="https://bulma.io/images/placeholders/128x128.png" >
+    </p>
+  </figure>
+  <div class="media-content">
+    <div class="content">
+      <p>
+        <strong> {{ Answer_name }} </strong>, {{ Answer_date }}
+        <br>
+          {{ Answer_text }}
+        <br>
+        <small><a>Marquer comme important</a> · <a>Répondre</a> · <a>Créer un fil à partir de commentaire</a> </small>
+      </p> <br />
+    </div>
+   </div>
+ </article>
+</div>
+
+</template>
+
+<script>
+export default {
+    props: ['Answer_name','Answer_date','Answer_text','Answer_image']
+}
+</script>
---
+++ Message.vue
@@ -0,0 +1,28 @@
+<template>
+
+<article class="media">
+  <figure class="media-left">
+    <p class="image is-64x64">
+      <img src="https://bulma.io/images/placeholders/128x128.png" >
+    </p>
+  </figure>
+  <div class="media-content">
+    <div class="content">
+      <p>
+        <strong> {{ Message_name }} </strong>, {{ Message_date }}
+        <br>
+          {{ Message_text }}
+        <br>
+        <small><a>Marquer comme important</a> · <a>Répondre</a> · <a>Créer un fil à partir de commentaire</a> </small>
+      </p> <br />
+    </div>
+   </div>
+</article>
+
+</template>
+
+<script>
+export default {
+    props: ['Message_name','Message_date','Message_text','Message_image']
+}
+</script>
