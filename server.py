8d657362db808436635c44169d265c0c24d766a5
Romain Liautaud <romain.liautaud@ens-lyon.fr>
2018-02-25 23:49:07 +0100

    First draft of the thread and message schemas.

--- server.py
+++ server.py
@@ -1,3 +1,64 @@
+"""
+A tozti extension providing discussion threads.
+"""
+__license__ = 'AGPL'
+__author__ = 'The tozti Project'
+__email__ = 'tozti@ens-lyon.fr'
+
+
+"""The schema for a discussion thread."""
+THREAD_SCHEMA = {
+    'body': {
+        'name': {
+            'type': 'string'
+        },
+        'messages': {
+            'type': 'relationship',
+            'arity': 'to-many',
+            'targets': 'discussion/message'
+        },
+        'important': {
+            'type': 'relationship',
+            'arity': 'to-many',
+            'targets': 'discussion/message'
+        },
+    }
+}
+
+"""The schema for a single message."""
+MESSAGE_SCHEMA = {
+    'body': {
+        'content': {
+            'type': 'string'
+        },
+        'author': {
+            'type': 'relationship',
+            'arity': 'to-one',
+            'targets': 'core/user'
+        },
+        'parent': {
+            'type': 'relationship',
+            'arity': 'to-one',
+            'targets': 'discussion/message'
+        },
+        'children': {
+            'type': 'relationship',
+            'arity': 'auto',
+            'pred-type': 'discussion/message',
+            'pred-relationship': 'parent'
+        }
+    }
+}
+
+
+# TODO(liautaud): We should uncomment the type declarations
+# once the new version of the storage API (which is available
+# under the `storage-refactor` branch) is merged into `master`.
 MANIFEST = {
+    'name': 'discussion',
     'includes': ['build.js', 'css/style.css'],
+    # 'types': {
+    #     'thread': THREAD_SCHEMA,
+    #     'message': MESSAGE_SCHEMA
+    # }
 }
