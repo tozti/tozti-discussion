1778a2d90f5cff9bef56b37d3c777f32619528b6
Blop <fel.klingel@gmail.com>
2018-02-25 21:46:31 +0100

    update of index.js to fit the new requirements

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import Discussion from './components/Discussion.vue';
 
 
 // add a new route with highest priority
-tozti.routes.unshift(
-  { path: '/discussion', component: Discussion }
-)
+
 tozti.addMenuItem("Discussion", "/discussion")
+tozti.addRoutes([
+  { path: '/discussion', component: Discussion }
+])
