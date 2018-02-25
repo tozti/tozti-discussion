942138180875eb3000d4b4f7b42f3c750c1a8343
Romain Liautaud <romain.liautaud@ens-lyon.fr>
2018-02-25 19:37:07 +0100

    Starting with a clean slate.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import Discussion from './components/Discussion.vue';
+'use strict'
+
+import Thread from './components/Thread.vue'
+
+// tozti.addRoutes([
+// 	{path: }
+// ])
 
 
-// add a new route with highest priority
-tozti.routes.unshift(
-  { path: '/discussion', component: Discussion }
-)
-tozti.addMenuItem("Discussion", "/discussion")
+// import Discussion from './components/Discussion.vue';
+
+
+
+
+// // add a new route with highest priority
+// tozti.routes.unshift(
+//   { path: '/discussion', component: Discussion }
+// )
+// tozti.addMenuItem("Discussion", "/discussion")
