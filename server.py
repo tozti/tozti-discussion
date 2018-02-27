5f94138630912cb6a3bbbecfae477265264d55b5
fklingel <fel.klingel@gmail.com>
2018-02-27 21:16:21 +0100

    added types to manifest (forgot to add file last time)

--- server.py
+++ server.py
@@ -1,3 +1,42 @@
 MANIFEST = {
     'includes': ['build.js', 'css/style.css'],
+    'types': {
+      'message': {
+        'body': {
+          'date': { 'type': 'date' },
+          'content': { 'type': 'string' },
+          'author': { 
+            'type': 'relationship',
+            'arity': 'to-one',
+            'targets': 'core/user'
+          },
+          'answers': { 
+            'type': 'relationship',
+            'arity': 'to-many',
+            'targets': 'tozti-discussion/answer'
+          }
+        }
+      }
+      'answer': {
+        'body': {
+          'date': { 'type': 'date' },
+          'content': { 'type': 'string' },
+          'author': { 
+            'type': 'relationship',
+            'arity': 'to-one',
+            'targets': 'core/user'
+          }
+        }
+      }
+      'thread': {
+        'body': {
+          'title': { 'type': 'string' },
+          'messages': {
+            'type': 'relationship',
+            'arity': 'to-many',
+            'targets': 'tozti-discussion/message',
+          }
+        }
+      }
+    }
 }
