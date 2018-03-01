c6a35410466cd8303980c9620b5002d7164360df
Romain Liautaud <romain.liautaud@ens-lyon.fr>
2018-03-01 23:37:40 +0100

    Added compatibility with the new taxonomy API.

--- src/index.js
+++ src/index.js
@@ -2,19 +2,6 @@
 
 import Thread from './components/Thread.vue'
 
-// TODO(liautaud): We don't actually need to declare a route.
-// Instead, we should use the dispatching system provided by
-// the API, which allows our component to be rendered when
-// any entity of type `thread` has to be displayed.
-// Sadly, this system is not yet implemented. So we wait.
-tozti.addRoutes([
-    {
-        path: '/discussion/:id',
-        component: Thread,
-        props: true
-    }
-])
-
 // Custom resource types.
 import TaxonomyItem from './components/TaxonomyItem.vue'
 import NewThreadForm from './components/Forms/NewThreadForm.vue'
@@ -22,5 +9,6 @@ tozti.addResourceType(
     'discussion/thread',
     'discussion', 'f', 
     TaxonomyItem,
+    Thread,
     NewThreadForm
 )
